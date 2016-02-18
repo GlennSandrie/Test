17ef6994ececafa0922c49e659399cbd19f4cc24
Anjana T'Jampens <anjana.tjampens@gmail.com>
2016-02-18 10:30:38 +0100

    Spel en Speler zijn toegevoegd

---
+++ src/domein/Speler.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domein;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author anjana
+ */
+public class Speler 
+{
+    private String spelerId;
+    private String voornaam;
+    private String familienaam;
+    private String adres;
+    private String email;
+    private String wachtwoord;
+    private LocalDate geboortedatum;
+    private int score;
+    private boolean adminrechten;
+}
