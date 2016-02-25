8581b172a4ac9c6a47c866d57050101ec2b277c0
Anjana T'Jampens <anjana.tjampens@gmail.com>
2016-02-25 10:11:06 +0100

    Mapper klasse aangemaakt + connectie databank

---
+++ src/persistentie/Connectie.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistentie;
+
+/**
+ *
+ * @author anjana
+ */
+public class Connectie 
+{
+    public static final String JDBC_URL = "jdbc:mysql://db4free.net:3306/dobbelsteen?user=p1g55&password=123456";
+}
---
+++ src/persistentie/SpelMapper.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistentie;
+
+import domein.Spel;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import static java.util.Collections.list;
+import java.util.List;
+
+/**
+ *
+ * @author anjana
+ */
+public class SpelMapper 
+{
+    public List<String> geefSpelnamen()
+    {
+        List<String> spelNamen = new ArrayList<>();
+        try (Connection conn = DriverManager.getConnection(Connectie.JDBC_URL))
+        {
+            PreparedStatement query = conn.prepareStatement("SELECT spelId FROM spel");
+            try (ResultSet rs = query.executeQuery()) {
+                while (rs.next()) {
+                    String spelId = rs.getString("spelId");
+                    spelNamen.add(spelId);
+                }
+            }
+        }catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+        
+        return spelNamen;
+    }
+    
+    public Spel kiesSpel(String naam)
+    {
+        Spel spel = null;
+
+        try (Connection conn = DriverManager.getConnection(Connectie.JDBC_URL)) {
+            PreparedStatement query = conn.prepareStatement("SELECT * FROM spel WHERE spelId = ?");
+            query.setString(1, naam);
+            try (ResultSet rs = query.executeQuery()) {
+                if (rs.next()) {
+                    spel = new Spel(naam);
+                }
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        return spel;
+    }
+}
