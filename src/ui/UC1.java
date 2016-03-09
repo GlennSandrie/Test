5a920848e88ff642bfd12ab0a977be622ae4e66d
Aïssa Peters <aissa.peters@gmail.com>
2016-03-09 10:43:08 +0100

    Merge origin/master
    Conflicts:
    	src/ui/UC1.java

--- src/ui/UC1.java
+++ src/ui/UC1.java
@@ -20,7 +20,7 @@ public class UC1 {
         Scanner input = new Scanner(System.in);
         String keuze = "";
         while (!spelnamen.contains(keuze)) {
-            System.out.println("Kies een spel uit de onderstaande lijst");
+            System.out.println(dc.getTaal().getText("bestaandSpel"));
             for (String spelnaam : spelnamen) {
                 System.out.println(spelnaam);
             }
@@ -32,48 +32,6 @@ public class UC1 {
         System.out.println(/*"De spelers van dit spel"*/dc.getTaal().getText("speler"));
         for (String spelersnaam : spelersnamen) {
             System.out.println(spelersnaam);
-        }
-    }
-}
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package ui;
-
-import domein.DomeinController;
-import java.util.List;
-import java.util.Scanner;
-
-/**
- *
- * @author beheerder
- */
-public class UC1 {
-    
-    public static void laadSpel(DomeinController dc) {
-        // lijst van spelnamen ophalen
-        List<String> spelnamen = dc.geefSpelnamen();
-        Scanner input = new Scanner(System.in);
-        String keuze = "";
-        while (!spelnamen.contains(keuze)) {
-            System.out.println(dc.getTaal().getText("bestaandSpel"));;
-            for (String spelnaam : spelnamen) {
-                System.out.println(spelnaam);
-            }
-            keuze = input.nextLine();
-        }
-        
-        // lijst van spelers ophalen
-        List<String> spelersnamen = dc.geefSpelersVanSpel(keuze);
-        if (spelersnamen.size()==0)
-           System.out.println("Er zitten geen spelers in dit spel");
-        else
-        {
-            System.out.println("De spelers van dit spel");
-            for (String spelersnaam : spelersnamen) {
-                System.out.println(spelersnaam);
-            }
         }
     }
 }
