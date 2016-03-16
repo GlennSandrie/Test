fd9b2ce7e7d7bdeafa175892911573c951a6c407
Lisa Van Wesemael <lisa.vanwesemael@outlook.be>
2016-03-16 16:16:47 +0100

    EnumSchat werd toegevoegd

--- src/utils/EnumSchat.java
+++ src/utils/EnumSchat.java
@@ -5,11 +5,30 @@
  */
 package utils;
 
+
 /**
  *
  * @author anjana
  */
 public enum EnumSchat 
 {
+
+ 
+    GOUDENMUNTEN(1),  VLEERMUIS(2), BOEK(3), DRAAK(4), GEESTFLES(5), 
+    GEESTZWAAI(6), KROON(7), KANDELAAR(8), RING(9), HELM(10), JUWEEL(11),
+    VARKEN(12), HAGEDIS(13), MOT(14), UIL(15), RAT(16), SCARABEE(17), 
+    SLEUTELS(18), SCHEDEL(19), TOVENARES(20), SPIN(21), ZWAARD(22),
+    SCHATKIST(23), SCHATKAART(24);
     
+    private final int schatId;
+    
+    EnumSchat(int schatId)
+    {
+        this.schatId=schatId;
+    }
+
+    public int getSchatId()
+    {
+        return schatId;
+    }
 }
