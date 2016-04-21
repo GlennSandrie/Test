ac2566686ec0af5262a00603d00bdf4262d5798e
Sabine De Vreese <sabine.devreese2@hogent.be>
2016-04-21 16:37:28 +0200

    Project nagekeken

--- src/domein/HoekKaart.java
+++ src/domein/HoekKaart.java
@@ -6,6 +6,8 @@
 package domein;
 
 import exceptions.WrongInputException;
+import java.util.ArrayList;
+import java.util.List;
 import utils.Kleur;
 import utils.Richting;
 import utils.Schat;
@@ -14,8 +16,7 @@ import utils.Schat;
  *
  * @author anjana
  */
-public class HoekKaart extends Gangkaart
-{
+public class HoekKaart extends Gangkaart {
 
     /**
      *
@@ -23,8 +24,7 @@ public class HoekKaart extends Gangkaart
      * @param kleur
      * @param richting
      */
-    public HoekKaart(Schat schat, Kleur kleur, Richting[] richting)
-    {
+    public HoekKaart(Schat schat, Kleur kleur, Richting richting) {
         super(null, kleur, richting);
     }
 
@@ -33,8 +33,7 @@ public class HoekKaart extends Gangkaart
      * @param schat
      * @param richting
      */
-    public HoekKaart(Schat schat, Richting[] richting)
-    {
+    public HoekKaart(Schat schat, Richting richting) {
         super(schat, richting);
     }
 
@@ -42,74 +41,78 @@ public class HoekKaart extends Gangkaart
      *
      * @param richting
      */
-    public HoekKaart(Richting[] richting)
-    {
+    public HoekKaart(Richting richting) {
         super(richting);
     }
 
-    Richting[] richtingen =
-    {
-        Richting.O, Richting.R
-    };
-    Richting[] richtingen2 =
-    {
-        Richting.O, Richting.L
-    };
-    Richting[] richtingen3 =
-    {
-        Richting.B, Richting.R
-    };
-    Richting[] richtingen4 =
-    {
-        Richting.B, Richting.L
-    };
-
-    public Gangkaart[] draaienKaart(int keuze)
-    {
-        Gangkaart[] draaiKaart = new Gangkaart[4];
-        System.out.printf("%s%20s%20s%20s%n", "1.", "2.", "3.", "4.");
-//
-        draaiKaart = new Gangkaart[4];
-
-        draaiKaart[0] = new HoekKaart(null, richtingen);
-
-        draaiKaart[1] = new HoekKaart(null, richtingen2);
-
-        draaiKaart[2] = new HoekKaart(null, richtingen3);
-
-        draaiKaart[3] = new HoekKaart(null, richtingen4);
-        super.printKaart();
-        return draaiKaart;
-
-    }
-    @Override
-    public Gangkaart geefGekozenKaart(int keuze)
-    {
-        Gangkaart gk = super.getKeuzeKaart();
-        if (keuze == 1)
-        {
-            gk = new HoekKaart(null, richtingen);
-        } else
-        {
-            if (keuze == 2)
-            {
-                gk = new HoekKaart(null, richtingen2);
-
-            } else
-            {
-                if (keuze == 3)
-                {
-                    gk = new HoekKaart(null, richtingen3);
-
-                } else if (keuze == 4)
-                {
-                    gk = new HoekKaart(null, richtingen4);
-
-                }
-            }
+    public String toString() {
+        String kaart = "";
+        switch (richting) {
+            case B:
+                kaart = String.format(
+                        "xxx      xxx"
+                        + "xxx      xxx"
+                        + "xxx         "
+                        + "xxx         "
+                        + "xxxxxxxxxxxx"
+                        + "xxxxxxxxxxxx");
+                break;
+            case L:
+                kaart = String.format(
+                        "xxxxxxxxxxxx"
+                        + "xxxxxxxxxxxx"
+                        + "xxx         "
+                        + "xxx         "
+                        + "xxx      xxx"
+                        + "xxx      xxx");
+                break;
+            case O:
+                kaart = String.format(
+                        "xxxxxxxxxxxx"
+                        + "xxxxxxxxxxxx"
+                        + "         xxx"
+                        + "         xxx"
+                        + "xxx      xxx"
+                        + "xxx      xxx");
+                break;
+            case R:
+                kaart = String.format(
+                        "xxx      xxx"
+                        + "xxx      xxx"
+                        + "         xxx"
+                        + "         xxx"
+                        + "xxxxxxxxxxxx"
+                        + "xxxxxxxxxxxx");
+                break;
         }
-        super.setKeuzeKaart(gk);
-        return gk;
+
+        if (schat != null) {
+            kaart = kaart.substring(0, 41) + "S" + kaart.substring(42);
+        }
+        return kaart;
+    }
+
+    public List<Richting> geefVerplaatsRichtingen() {
+        ArrayList<Richting> al = new ArrayList();
+        switch (richting) {
+            case B:
+                al.add(Richting.B);
+                al.add(Richting.L);
+                break;
+            case L:
+                al.add(Richting.L);
+                al.add(Richting.O);
+                break;
+            case O:
+                al.add(Richting.O);
+                al.add(Richting.R);
+                break;
+            case R:
+                al.add(Richting.R);
+                al.add(Richting.O);
+                break;
+        }
+        return al;
     }
 
 }
